bf1cf7b57ec9a328ab90ffb12a58376b28da476b
Madi Abio <madelineabio@gmail.com>
2025-09-26 09:30:19 +1000

    Installed fuzzy finder

--- nvim/init.lua
+++ nvim/init.lua
@@ -56,6 +56,22 @@ require("lazy").setup({
   },
 
   {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git/" },
+          layout_config = {
+            horizontal = { preview_width = 0.6 },
+          },
+        },
+      })
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
@@ -139,6 +155,14 @@ cmp.setup({
     { name = "luasnip" },
   },
 })
+
+-- ===== Telescope keymaps =====
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
 
 -- ===== Claude Code keymaps =====
 vim.keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<cr>", { desc = "Open Claude Code" })
