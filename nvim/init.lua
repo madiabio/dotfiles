bd5b974b68cc3e01fbdb0ae555826ed0dbf703be
Madi Abio <madelineabio@gmail.com>
2025-10-11 10:53:57 +1000

    added shade

--- nvim/init.lua
+++ nvim/init.lua
@@ -126,6 +126,20 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "sunjon/Shade.nvim",
+    config = function()
+      require("shade").setup({
+        overlay_opacity = 70,
+        opacity_step = 1,
+        keys = {
+          brightness_up   = "<C-Up>",
+          brightness_down = "<C-Down>",
+          toggle          = "<Leader>s"
+        }
+      })
+    end
+  }
 })
 -- ===== Mason (LSP installer) =====
 require("mason").setup()
